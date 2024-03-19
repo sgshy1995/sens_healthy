9d9d5874d4cf6e7f10f14a7c3efc21acb5b0cc14
EdenSheng <singlesaulwork@gmail.com>
2024-03-19 13:45:44 +0800

    update: remove not use dart files

--- lib/iconfont.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Iconfont {
-  static const IconData phone =
-      IconData(0xe66b, fontFamily: "IconFont", matchTextDirection: true);
-
-  static const IconData qc =
-      IconData(0xe66d, fontFamily: "IconFont", matchTextDirection: true);
-}
